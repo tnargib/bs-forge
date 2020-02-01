d82542159d09fca02eb980eda59f186fe6b1802d
Simon Brigant <simon.brigant@open-groupe.com>
2020-02-01 22:41:54 +0100

    inifinte scroll songs

--- src/containers/SongsContainer.tsx
+++ src/containers/SongsContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { toJS, isObservable, isObservableArray } from "mobx";
 import { observer } from "mobx-react";
 
 import { useStores } from "../services/mobx/useStores";
@@ -11,13 +12,20 @@ const SongsContainer: React.FC = () => {
   const { songStore } = useStores();
 
   useEffect(() => {
+    console.log("useEffect SongsContainer");
     songStore.loadSongs(SongSortOrder.Rating);
   }, [songStore]);
+
+  const loadSongs = (page = 0) => {
+    return songStore.loadSongs(SongSortOrder.Rating, page);
+  };
+
+  console.log("rerender");
 
   return (
     <Switch>
       <Route path="/songs/rating">
-        <SongList songs={songStore.allSongs} />
+        <SongList pages={songStore.songPages} loadSongs={loadSongs} />
       </Route>
       <Redirect to="/songs/rating" />
     </Switch>
--- src/views/Songs/SongList/SongList.tsx
+++ src/views/Songs/SongList/SongList.tsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import cx from "classnames";
-import { pipe, keys, equals, reject, path } from "ramda";
+import { pipe, keys, equals, reject, path, flatten, pluck, last } from "ramda";
 
-import { Song } from "../../../services/apis/BeatSaverApi";
+import { Song, SongPage } from "../../../services/apis/BeatSaverApi";
 
 import useStyles from "./SongListStyle";
 
 type Props = {
-  songs: Song[];
+  pages: SongPage[];
+  loadSongs: (page?: number) => void;
 };
 
-const SongList: React.FC<Props> = props => {
+const SongList: React.FC<Props> = ({ pages, loadSongs }) => {
   const classes = useStyles();
 
+  useEffect(() => {
+    const fetchNewPage = () => {
+      const lastPage: SongPage = last(pages);
+      if (lastPage.nextPage) loadSongs(lastPage.nextPage);
+    };
+
+    const handleScroll = () => {
+      const root = document.documentElement;
+      console.log(window.innerHeight, root.scrollTop, root.offsetHeight);
+      if (window.innerHeight + root.scrollTop !== root.offsetHeight) return;
+      console.log("Fetch more list items!");
+      fetchNewPage();
+    };
+
+    console.log("useEffect: SongList handleScroll");
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [pages, loadSongs]);
+
+  const allSongs: Song[] = flatten(pluck("docs", pages));
   const difficulties = pipe(path(["metadata", "difficulties"]), reject(equals(false)), keys);
 
   return (
     <>
-      {props.songs.map(song => (
+      {allSongs.map(song => (
         <div key={song._id} className={cx(classes.songItem)}>
           <img
             className={cx(classes.cover)}
